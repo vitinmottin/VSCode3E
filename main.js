e2ae29c805e709ca0b51ad8afd8e44d3e37cf90f
vitinmottin <166723677+vitinmottin@users.noreply.github.com>
2024-05-17 07:53:24 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
+
 for(let i=0;i<botoes.length;i++){
     botoes[i].onclick=function(){
-        botoes[i].classList.add("ativo");
+        for(let j=0;j<botoes.length;j++){
+            botoes[j].classList.remove("ativo");
+            botoes[i].classList.add("ativo");
+        }
+       botoes[i].classList.add("ativo");
+       textos[i].classList.add("ativo");
     }
 }
+const contadores = document.querySelectorAll(".contador");
+const tempoObijetivo1 = new Date("2024-06-17TOO:00:00");
+contadores[0].textContent = "contagem regressiva";
